771744f7752186457747ea2d6a6c280fb38a2f2a
joshraiborde <joshraiborde@gmail.com>
2021-07-21 18:10:51 -0400

    changed styling

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
+
+
+marked.setOptions({
+    breaks: true
+})
+
+const renderer = new marked.Renderer();
+
 function App() {
     const [text, setText] = React.useState("");
 
     return(
         <div className="text-center px-4">
             <h1 className="p-4">The Markdown Previewer!</h1>
+            <div className="row">
+            <div className="col-6">
+
+            <h5 className="text-center text-white">ENTER MARKDOWN HERE:</h5>
             <textarea
                 name="text"
                 id="text"
                 rows="10"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
-                className="textarea"
-            ></textarea>
-            <h3 className="mt-3">OUTPUT</h3>
+                className="textarea">
+                </textarea>
+            </div>
+            <div className="col-6">
+
+            <h5 className="mt-3">MARKDOWN TRANSFORMED:</h5>
             <Preview markdown={text} />
+            </div>
+            </div>
         </div>
     )
 
 }
 function Preview({ markdown }) {
-    return <div> </div>
+    return <div
+
+    dangerouslySetInnerHTML={{
+        __html: marked(markdown, { renderer: renderer}),
+    }}
+    id="preview"
+    ></div>
 }
 
 ReactDOM.render(<App />, document.getElementById("root") );
